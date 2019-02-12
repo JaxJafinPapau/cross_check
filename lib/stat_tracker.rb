31c36bec3c459f83aba4e48f649c7c38277cdf65
Alek <alekaker@outlook.com>
2019-02-12 13:47:02 -0700

    refactoring code with new class for each stat category

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,8 +1,39 @@
-
+require 'csv'
+require 'pry'
+require './lib/mather'
 
 class StatTracker
+  include Mather
 
-  def initialize
-
+  def self.from_csv(file)
+    CSV.read(file)
   end
+
+  # def highest_total_score(game_file)
+  #   high_scores = adder(game_file, 6, 7)
+  #   high_scores.max
+  # end
+  #
+  # def lowest_total_score(game_file)
+  #   low_scores = adder(game_file, 6, 7)
+  #   low_scores.drop(1).min
+  # end
+  #
+  # def biggest_blowout(game_file)
+  #   blowouts = differencer(game_file, 6, 7)
+  #   blowouts.max
+  # end
+
+  # def percentage_home_wins(game_file)
+  #   total_games = CSV.read(game_file).count
+  #   home_game_wins = element_counter(game_file, "home win")
+  #   percentager(total_games, home_game_wins)
+  # end
+  #
+  # def matching_element_counter(game_file, string)
+  #   x = CSV.foreach(game_file) do |element|
+  #     element.match(string)
+  #   end
+  #   x.count
+  # end
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,9 +1,14 @@
 require './lib/stat_tracker'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/mather'
+require 'csv'
 
 class StatTrackerTest < Minitest::Test
   def setup
+    @game_test_file = './data/fixture_game.csv'
+    # team_test_path = './data/fixture_team_info.csv'
+    # game_teams_test_path = './data/fixture_game_teams_stats.csv'
     @stat_tracker = StatTracker.new
   end
 
@@ -11,7 +16,24 @@ class StatTrackerTest < Minitest::Test
     assert_instance_of StatTracker, @stat_tracker
   end
 
+  def test_stat_tracker_can_get_csv_files
+    assert_equal "game_id", StatTracker.from_csv(@game_test_file).first.first
+  end
+
   def test_stat_tracker_has_highest_total_score
-    assert_equal 0, @stat_tracker.highest_total_score
+    assert_equal 7, @stat_tracker.highest_total_score(@game_test_file)
+  end
+
+  def test_stat_tracker_has_lowest_total_score
+    assert_equal 3, @stat_tracker.lowest_total_score(@game_test_file)
+  end
+
+  def test_stat_tracker_gives_biggest_blowout
+    assert_equal 3, @stat_tracker.biggest_blowout(@game_test_file)
+  end
+
+  def test_stat_tracker_gives_percentage_home_wins
+    skip
+    assert_equal 66.67, @stat_tracker.percentage_home_wins(@game_test_file)
   end
 end
