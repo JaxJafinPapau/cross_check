8168afb2e4c2d4e5711ca052bd82c6cb4ef2a5bd
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-11 17:50:25 -0700

    Adds from_csv Class method, adds highest_total_score method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,8 +1,21 @@
-
+require 'csv'
 
 class StatTracker
 
-  def initialize
+  # def initialize
+  #
+  # end
 
+  def self.from_csv(file)
+    CSV.read(file)
+  end
+
+  def highest_total_score(game_file)
+    all_scores = []
+    CSV.foreach(game_file) do |game|
+      total_score = game[6].to_i + game[7].to_i
+      all_scores << total_score
+    end
+    all_scores.max
   end
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,9 +1,13 @@
 require './lib/stat_tracker'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'csv'
 
 class StatTrackerTest < Minitest::Test
   def setup
+    @game_test_file = './data/fixture_game.csv'
+    # team_test_path = './data/fixture_team_info.csv'
+    # game_teams_test_path = './data/fixture_game_teams_stats.csv'
     @stat_tracker = StatTracker.new
   end
 
@@ -11,7 +15,11 @@ class StatTrackerTest < Minitest::Test
     assert_instance_of StatTracker, @stat_tracker
   end
 
+  def test_stat_tracker_can_get_csv_files
+    assert_equal "game_id", StatTracker.from_csv(@game_test_file).first.first
+  end
+
   def test_stat_tracker_has_highest_total_score
-    assert_equal 0, @stat_tracker.highest_total_score
+    assert_equal 7, @stat_tracker.highest_total_score(@game_test_file)
   end
 end
