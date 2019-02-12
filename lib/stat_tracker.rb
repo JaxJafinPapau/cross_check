ee7440b3a493c25c544d73b7d1f35fee8017cf6c
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-12 10:04:45 -0700

    Adds mather module, refactors stat_tracker methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,10 +1,12 @@
 require 'csv'
 require 'pry'
+require './lib/mather'
 
 class StatTracker
+  include Mather
 
-  # def initialize
-  #
+  # def initialize(file_path)
+  #   @data_set = file_path
   # end
 
   def self.from_csv(file)
@@ -12,20 +14,16 @@ class StatTracker
   end
 
   def highest_total_score(game_file)
-    high_scores = []
-    CSV.foreach(game_file) do |game|
-      total_score = game[6].to_i + game[7].to_i
-      high_scores << total_score
-    end
+    high_scores = adder(game_file, 6, 7)
     high_scores.max
   end
 
   def lowest_total_score(game_file)
-    low_scores = []
-    CSV.foreach(game_file) do |game|
-      total_score = game[6].to_i + game[7].to_i
-      low_scores << total_score
-    end
+    low_scores = adder(game_file, 6, 7)
     low_scores.drop(1).min
+  end
+
+  def method_name
+
   end
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,6 +1,7 @@
 require './lib/stat_tracker'
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/mather'
 require 'csv'
 
 class StatTrackerTest < Minitest::Test
