bdce980f12d0c5498c268e79785eb6250926f022
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-14 15:06:25 -0700

    completes attributes test. completes test file

--- test/game_team_stats_test.rb
+++ test/game_team_stats_test.rb
@@ -25,3 +25,22 @@ class GameTeamStatsTest < Minitest::Test
       }
     @game_team_stats = GameTeamStats.new(categories)
   end
+
+  def test_game_team_goals_has_attributes
+    assert_equal "2012030221", @game_team_stats.game_id
+    assert_equal "3", @game_team_stats.team_id
+    assert_equal "away", @game_team_stats.hoa
+    assert_equal "FALSE", @game_team_stats.won
+    assert_equal "OT", @game_team_stats.settled_in
+    assert_equal "John Tortorella", @game_team_stats.head_coach
+    assert_equal 2, @game_team_stats.goals
+    assert_equal 35, @game_team_stats.shots
+    assert_equal 44, @game_team_stats.hits
+    assert_equal 8, @game_team_stats.pim
+    assert_equal 3, @game_team_stats.powerPlayOpportunities
+    assert_equal 0, @game_team_stats.powerPlayGoals
+    assert_equal 44.8, @game_team_stats.faceOffWinPercentage
+    assert_equal 17, @game_team_stats.giveaways
+    assert_equal 7, @game_team_stats.takeaways
+  end
+end
