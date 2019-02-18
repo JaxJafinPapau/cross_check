1042969f7472edc9e359994adc83ff155b78ca3d
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-18 16:26:57 -0700

    debugs game methods

--- lib/game_methods.rb
+++ lib/game_methods.rb
@@ -1,5 +1,4 @@
 module GameMethods
-
   def highest_total_goals
     games.map {|game| game.home_goals.to_i + game.away_goals.to_i}.max
   end
@@ -27,19 +26,20 @@ module GameMethods
   end
 
   def count_of_games_by_season
-    binding
     games_by_season = @games.group_by { |game| game.season }
     games_by_season.each { |season, games| games_by_season[season] = games.count }
+  end
 
   def average_goals_per_game #across all seasons
     goals = games.map {|game| game.home_goals.to_i + game.away_goals.to_i}.sum
     (goals / games.count.to_f)
   end
-    
+
   def average_goals_by_season
     goals_by_season = @games.group_by { |game| game.season }
     goals_by_season.each do |season, games|
     goals = games.map {|game| game.home_goals.to_i + game.away_goals.to_i}.sum
     goals_by_season[season] = (goals.to_f / games.count.to_f).round(2)
+    end
   end
 end
