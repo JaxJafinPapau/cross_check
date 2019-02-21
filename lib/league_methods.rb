d075e2601b5a3e8600f7b63df9bc9b5978455d1c
jaxjafinpapau <jeremy.bennett8@gmail.com>
2019-02-20 21:16:29 -0700

    debug

--- lib/league_methods.rb
+++ lib/league_methods.rb
@@ -2,7 +2,6 @@ require "pry"
 require './lib/game_methods'
 
 module LeagueMethods
-
   def total_number_of_teams
     team_info_rows.count
   end
@@ -13,55 +12,40 @@ module LeagueMethods
       x = @game_team_stats.select { |game| game.team_id == team.team_id }
       y = x.reduce(0) { |sum, n| sum + n.goals.to_i }
       average_score = y / games.count.to_f
-      average_scores[team.teamname] = average_score
+      average_scores[team.team_name] = average_score
     end
     average_scores.max_by { |team, score| score }.first
   end
 
-    # Name of the team with the highest average number of goals scored per game across all seasons.
   def worst_offense
-      average_scores = {}
-      @team_info_rows.each do |team|
-        x = @game_team_stats.select { |game| game.team_id == team.team_id }
-        y = x.reduce(0) { |sum, n| sum + n.goals.to_i }
-        average_score = y / games.count.to_f
-        average_scores[team.teamname] = average_score
-      end
-      average_scores.min_by { |team, score| score }.first
+    average_scores = {}
+    @team_info_rows.each do |team|
+      x = @game_team_stats.select { |game| game.team_id == team.team_id }
+      y = x.reduce(0) { |sum, n| sum + n.goals.to_i }
+      average_score = y / games.count.to_f
+      average_scores[team.team_name] = average_score
     end
+    average_scores.min_by { |team, score| score }.first
+  end
 
   def best_defense
     goals_allowed = Hash.new(0)
-       games_matches = @game_team_stats.group_by { |game| game.game_id}
-       # x = @game_team_stats.select { |game| game.team_id == team.team_id }
-       x = @game_team_stats.map {|game| game.team_id}
-
-       games_matches.values.each { |pair| goals_allowed[pair[1].team_id] += pair[0].goals.to_i  }
-       games_matches.values.each { |pair| goals_allowed[pair[0].team_id] += pair[1].goals.to_i  }
-       y = goals_allowed.min_by {|team, values| values / (x.count(team)/2.0) }
-       team_id_converter(y[0])
-
-    #Name of the team with the lowest average number of goals allowed per game across all seasons.
+    games_matches = @game_team_stats.group_by { |game| game.game_id}
+    games_matches.values.each { |pair| goals_allowed[pair[1].team_id] += pair[0].goals.to_i  }
+    games_matches.values.each { |pair| goals_allowed[pair[0].team_id] += pair[1].goals.to_i  }
+    x = @game_team_stats.map {|game| game.team_id}
+    y = goals_allowed.min_by {|team, values| values / (x.count(team)/2.0) }
+    team_id_converter(y[0])
   end
-
-  def team_id_converter(team_id_number)
-      favorite_team_row = @team_info_rows.find do |row|
-        row.team_id == team_id_number
-      end
-      favorite_team_row.teamname
-    end
 
   def worst_defense
     goals_allowed = Hash.new(0)
-       games_matches = @game_team_stats.group_by { |game| game.game_id}
-       # x = @game_team_stats.select { |game| game.team_id == team.team_id }
-       x = @game_team_stats.map {|game| game.team_id}
-
-       games_matches.values.each { |pair| goals_allowed[pair[1].team_id] += pair[0].goals.to_i  }
-       games_matches.values.each { |pair| goals_allowed[pair[0].team_id] += pair[1].goals.to_i  }
-       y = goals_allowed.max_by {|team, values| values / (x.count(team)/2.0) }
-       team_id_converter(y[0])
-    #Name of the team with the highest average number of goals allowed per game across all seasons.
+      games_matches = @game_team_stats.group_by { |game| game.game_id}
+      games_matches.values.each { |pair| goals_allowed[pair[1].team_id] += pair[0].goals.to_i  }
+      games_matches.values.each { |pair| goals_allowed[pair[0].team_id] += pair[1].goals.to_i  }
+      x = @game_team_stats.map {|game| game.team_id}
+      y = goals_allowed.max_by {|team, values| values / (x.count(team)/2.0) }
+      team_id_converter(y[0])
   end
 
   def highest_scoring_visitor
@@ -80,18 +64,18 @@ module LeagueMethods
 
     #Name of the team with the highest average score per game across all seasons when they are
   def highest_scoring_home_team
-      x = @game_team_stats.map do |game|
-         game.hoa == "away"
-         game.team_id
-       end
-      goals_scored_visitor = Hash.new(0)
-       @game_team_stats.each do |games|
-           games.hoa == "away"
-        goals_scored_visitor[games.team_id] += games.goals.to_i
-        end
-        y = goals_scored_visitor.max_by {|team, values| values / x.count(team) }
-        team_id_converter(y[0])
+    x = @game_team_stats.map do |game|
+       game.hoa == "away"
+       game.team_id
+     end
+    goals_scored_visitor = Hash.new(0)
+      @game_team_stats.each do |games|
+         games.hoa == "away"
+      goals_scored_visitor[games.team_id] += games.goals.to_i
       end
+    y = goals_scored_visitor.max_by {|team, values| values / x.count(team) }
+    team_id_converter(y[0])
+  end
     #Name of the team with the highest average score per game across all seasons when they are home.
 
 
